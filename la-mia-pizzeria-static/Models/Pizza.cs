962c2a8bc864a7f33193b5af876148b6ab98756d
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-05-10 16:30:23 +0200

    feat: layout

--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -2,16 +2,16 @@
 {
     public class Pizza
     {
-        public string _mame { get; set; }
+        public string _name { get; set; }
         public string _description { get; set; }
         public string _img { get; set; }
         public decimal _price { get; set; }
 
         public Pizza() { }
 
-        public Pizza(string mame, string description, string img, decimal price)
+        public Pizza(string name, string description, string img, decimal price)
         {
-            _mame = mame;
+            _name = name;
             _description = description;
             _img = img;
             _price = price;
