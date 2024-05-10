69b2866bfd8de1fe9df97563435e416fcc04e7f9
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-05-10 14:33:44 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string _mame { get; set; }
+        public string _description { get; set; }
+        public string _img { get; set; }
+        public decimal _price { get; set; }
+
+        public Pizza() { }
+
+        public Pizza(string mame, string description, string img, decimal price)
+        {
+            _mame = mame;
+            _description = description;
+            _img = img;
+            _price = price;
+        }
+    }
+}
